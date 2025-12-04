66384db06fad0a32490cc7e1de1b22da2af30ffb
Sagiri-lzumi <75474771+Sagiri-lzumi@users.noreply.github.com>
2025-12-04 17:22:21 +0800

    First submit
    submit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PySide6.QtCore import QThread
 from config import init_directories
 from ui.splash import IntroScreen
 from ui.main_window import MainWindow
-
+#first
 
 def main():
     init_directories()
